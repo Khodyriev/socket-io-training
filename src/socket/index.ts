b2d67249ea776ff221fd83908d1a256fbf8b264e
Hennadiy Khodyriev <khodirevgennadiy@gmail.com>
2022-07-10 18:27:44 +0300

    user connecting to server and uerName cheking

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,28 @@
 import { Server } from 'socket.io';
 import * as config from './config';
 
+const activeUsers: Array<string> = [];
+const rooms: Array<string> = [];
+
 export default (io: Server) => {
 	io.on('connection', socket => {
-		const username = socket.handshake.query.username;
-	});
+		const username = socket.handshake.query.username as string;
+		console.log(`${username} connected`);		
+			if (activeUsers.indexOf(username) == -1) {
+				activeUsers.push(username);
+				socket.on("disconnect", () => {			
+					console.log(`${username} disconnected`);
+					const disconnectIndex: number = activeUsers.indexOf(username);
+					activeUsers.splice(disconnectIndex, 1);
+					console.log(activeUsers);
+				  });
+			} else {
+				socket.emit("USER_EXIST", "User already exist.");
+				console.log("USER_EXIST");
+			};
+			console.log(activeUsers);		
+	});	
 };
+
+
+
